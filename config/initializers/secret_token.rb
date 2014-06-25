8bf0166f529e29f55b7fa86483ef6eb2b285179c
Steve Sikoryak <stevein3d@gmail.com>
2014-06-25 17:43:53 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Paradox::Application.config.secret_key_base = 'c3a04785370a528cbe50e800a637e07b73baf3bac087e0e56775d2fb32856a277abf10b6c67d62e3fde6631e9c15f4ddad0aa6813f2d543830a35565ab10e45a'
