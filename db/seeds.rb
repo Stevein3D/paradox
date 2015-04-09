ef558fe6a255fbe1ef23d19a740179775a4354ac
Steve Sikoryak <stevein3d@gmail.com>
2015-04-09 17:36:47 -0400

    Update seed information

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "Steve Sikoryak",
+             email: "stevein3d@gmail.com",
+             password:              "123456",
+             password_confirmation: "123456",
              admin: true)
 
-99.times do |n|
+9.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -21,3 +21,12 @@ User.create!(name:  "Example User",
                password:              password,
                password_confirmation: password)
 end
+
+Tagline.create!(source: "A.J.'s Time Travelers"
+                line:   "Knowledge Is Power")
+
+Tagline.create!(source: "About Time"
+                line:   "What if every moment in life came with a second chance?")
+
+Tagline.create!(source: "About Time"
+                line:   "A new funny film about love. With a bit of time travel.")
