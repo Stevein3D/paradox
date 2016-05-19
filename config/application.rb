d5e58c9b8c30cbb8a1c2358c9c295e3bc13e17fb
Steve Sikoryak <Stevein3d@gmail.com>
2016-05-19 18:43:47 -0400

    I think I did it

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Paradox
     # config.i18n.default_locale = :de
 
     # Serve static assets to Heroku
-    config.serve_static_files = true
+    # config.serve_static_files = true
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
